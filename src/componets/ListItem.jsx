3ac8ad5e6f4e6fa14a64d6b5df9e7d9569d41fc1
Joe-Mwangi <joem99271@gmail.com>
2023-02-14 09:36:33 +0300

    added icons

--- src/componets/ListItem.jsx
+++ src/componets/ListItem.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { AppContext } from "../context/Contex"
+import { FaEdit, FaTrash } from "react-icons/fa"
 
 function ListItem({list}) {
   const {deleteItem, editItem} = useContext(AppContext)
@@ -8,8 +9,10 @@ function ListItem({list}) {
     <div className="grocery-item">
         <p className="title">{list.text}</p>
         <div className="btn-container">
-        <button className="edit-btn" onClick={() => editItem(list)}>edsit</button>
-        <button className="delete-btn" onClick={() => deleteItem(list.id)}>delete</button>
+        {/* <button className="edit-btn" onClick={() => editItem(list)}></button> */}
+        {/* <button className="delete-btn" onClick={() => deleteItem(list.id)}>delete</button> */}
+        <FaEdit color="hsl(125, 67%, 44%)" size={18} onClick={() => editItem(list)}/>
+        <FaTrash color="hsl(360, 67%, 44%)" size={18} onClick={() => deleteItem(list.id)}/>
         </div>
     </div>
   )
